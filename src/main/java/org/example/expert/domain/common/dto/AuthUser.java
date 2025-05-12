04bc81f44f538e4263abe6902481a8aa2b7ddcab
pleasebelieveme <cain1227@naver.com>
2025-05-12 12:47:44 +0900

    fix: 레벨1-2 JWT의 이해 (Auth수정)

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -10,9 +10,10 @@ public class AuthUser {
     private final String email;
     private final UserRole userRole;
 
-    public AuthUser(Long id, String email, UserRole userRole) {
+    public AuthUser(Long id, String email, String nickname, UserRole userRole) {
         this.id = id;
         this.email = email;
+        this.nickname = nickname;
         this.userRole = userRole;
     }
 }
